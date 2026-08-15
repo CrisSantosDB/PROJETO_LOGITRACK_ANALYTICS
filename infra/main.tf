330a13b17bf5660b03a3c6723c7c2a4ccd770865
CrisSantosDB <crislesantos1830@gmail.com>
2026-08-15 20:38:02 -0300

    fix(infra): configure remote backend for terraform state

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,17 @@
 terraform {
+  required_version = ">= 1.8.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.100.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-logitrack-dev-brazilsouth"
+    storage_account_name = "stlogitrackdevbrazsouth"
+    container_name       = "tfstate"
+    key                  = "logitrack.terraform.tfstate"
   }
 }
 
